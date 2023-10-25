5f236c7422da4a0550f23a6fbf9e156ba404720e
Dang Hieu <danghieutieumai@gmail.com>
2023-10-25 18:32:28 +0700

    useEffect with Dom events

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import { useState } from 'react'
-const Course = [
-  {
-    id: 1,
-    name: 'HTML, Css'
-  },
-  {
-    id: 2,
-    name: 'Javascript'
-  },
-  {
-    id: 3,
-    name: 'ReactJS'
-  }
-]
+import Content from './content.js'
 function App() {
-  const [checked, setChecked] = useState([])
-  console.log(checked)
-  const handlRadio = () => {
-
-  }
-  const handleChecked = (id) => {
-    setChecked([...checked, id]
-    )
-    console.log(checked)
-  }
+  const [show, setShow] = useState(false)
   return (
     <div className="App" style={{ padding: '20px'}}>
-      {Course.map((course) => (
-        <div key={course.id}>
-          <input type='checkbox' 
-          onChange={() => handleChecked(course.id)} 
-          checked={ checked === course.id }
-          />
-          <label>{course.name}</label>
-        </div>
-      ))}
-
-      <button onClick={handlRadio}>Click</button>
+      <button onClick={() => setShow(!show)}>Togger</button>
+       {show && <Content/> } 
     </div>
   );
 }
